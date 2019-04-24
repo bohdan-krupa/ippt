b7f379826f2c8f870678ea0867810432eccf4648
Bodya <bodya.save.dev@gmail.com>
2019-04-24 21:08:43 +0300

    update

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
     <div class="container">
-    <!--   <div v-if="!asked && !tasked">
-        
-      </div> -->
-      <div v-if="asked">
-        <p>Країна-виробник</p>
-        <input v-model="machineType.country" type="text">
-        <p>Рік випуску</p>
-        <input v-model="machineType.year" type="number" min="1980">
-        <p>Марка</p>
-        <input v-model="machineType.mark" type="text">
-        <div @click="onDone" class="sign-btn">Готово</div>
+      <div v-if="!asked && !tasked">
+        <div @click="onAsk" class="sign-btn">Ask for repair</div>
+        <div @click="onMyTasks" class="sign-btn">My tasks</div>
       </div>
+      <router-view></router-view>
       <div v-if="tasked">
         <p>Machine:</p>
         <h4>{{ getType.country }}</h4>
--- src/components/NewRapair.vue
+++ src/components/NewRapair.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <div @click="onAsk" class="sign-btn">Ask for repair</div>
-    <div @click="onMyTasks" class="sign-btn">My tasks</div>
+  <div class="container">
+    <p>Країна-виробник</p>
+    <input v-model="machineType.country" type="text">
+    <p>Рік випуску</p>
+    <input v-model="machineType.year" type="number" min="1980">
+    <p>Марка</p>
+    <input v-model="machineType.mark" type="text">
+    <div @click="onDone" class="sign-btn">Готово</div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        asked:  false,
+        tasked: false,
+        uId:    null,
+        machineType: {
+          country: null,
+          year:    null,
+          mark:    null
+        }
+      }
+    }
+  }
+</script>
