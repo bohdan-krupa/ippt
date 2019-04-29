e33a6a9121d9af56f9ed2e7e71ad4e1f07b962af
Bodya <bodya.save.dev@gmail.com>
2019-04-29 18:13:40 +0300

    update

---
+++ src/components/ManagerAnswer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <p>Machine:</p>
+    <h4>{{ machine.country }}</h4>
+    <h4>{{ machine.year }}</h4>
+    <h4>{{ machine.mark }}</h4>
+  </div>
+</template>
