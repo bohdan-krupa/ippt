bc817440406de268deddb892129516306d12f50e
Bodya <bodya.save.dev@gmail.com>
2019-04-29 18:28:47 +0300

    update

--- src/components/ManagerAnswer.vue
+++ src/components/ManagerAnswer.vue
@@ -4,5 +4,13 @@
     <h4>{{ machine.country }}</h4>
     <h4>{{ machine.year }}</h4>
     <h4>{{ machine.mark }}</h4>
+
+    <p>Країна-виробник</p>
+    <input v-model="country" type="text">
+    <p>Рік випуску</p>
+    <input v-model="year" type="number" min="1980">
+    <p>Марка</p>
+    <input v-model="mark" type="text">
+    <div @click="onDone" class="sign-btn">Готово</div>
   </div>
 </template>
