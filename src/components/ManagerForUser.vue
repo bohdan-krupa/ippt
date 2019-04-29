78c224da25b38970b8d0d4cd5a439e4ddacffbf6
Bodya <bodya.save.dev@gmail.com>
2019-04-29 17:53:26 +0300

    update

--- src/components/ManagerForUser.vue
+++ src/components/ManagerForUser.vue
@@ -1,3 +1,30 @@
 <template>
-	<p>sdgns</p>
+  <div class="container">
+    <p>User: {{ email }}</p>
+    <p>Repairs:</p>
+    <div v-for="(machine, index) in machines" :key="index">
+      <p>Machine:</p>
+      <h4>{{ machine.country }}</h4>
+      <h4>{{ machine.year }}</h4>
+      <h4>{{ machine.mark }}</h4>
+    </div>
+  </div>
 </template>
+
+<script>
+  import firebase from 'firebase'
+
+  export default {
+    data() {
+      return {
+        machines: [],
+        email:    null
+      }
+    },
+    created() {
+      firebase.database().ref('clients').once('value', snap => {
+        console.log(snap.val()[this.$route.params.id])
+      })
+    }
+  }
+</script>
