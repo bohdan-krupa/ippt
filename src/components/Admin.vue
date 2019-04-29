996dbb70b8c3b31de8ab3a388f9ff73d5bf736e7
Bodya <bodya.save.dev@gmail.com>
2019-04-29 14:41:21 +0300

    update

---
+++ src/components/Admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Managers:</p>
+  <p v-for="manager in managers">{{ manager }}</p>
+  <p>Users:</p>
+  <p v-for="user in users">{{ user }}</p>
+</template>
+
+<script>
+  import firebase from 'firebase'
+
+  export default {
+    
+  }
+</script>
