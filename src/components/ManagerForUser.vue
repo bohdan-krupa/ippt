6d09d80aca816718698d20c0cc2d99bb96f868d7
Bodya <bodya.save.dev@gmail.com>
2019-04-30 14:43:31 +0300

    update

--- src/components/ManagerForUser.vue
+++ src/components/ManagerForUser.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-    <p>User: {{ email }}</p>
+    <h3>User: {{ email }}</h3>
     <p>Repairs:</p>
     <div v-for="(machine, index) in machines" :key="index">
       <p>Machine:</p>
       <h4>{{ machine.country }}</h4>
       <h4>{{ machine.year }}</h4>
       <h4>{{ machine.mark }}</h4>
+      <router-link :to="'/set-repair-type/'" class="sign-btn">Set repair type</router-link>
+      <!-- <router-link :to="" class="sign-btn">To repair</router-link> -->
     </div>
   </div>
 </template>
