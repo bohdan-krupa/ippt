ae2fe933a1dbc90a0a6322731b43cf7740e0fc2c
Bodya <bodya.save.dev@gmail.com>
2019-05-13 19:25:28 +0300

    update

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import firebase from 'firebase'
-
 import Authorization from '../src/components/Authorization.vue'
 import SignIn from '../src/components/SignIn.vue'
 import SignUp from '../src/components/SignUp.vue'
@@ -21,42 +19,14 @@ const router = new VueRouter({
     { path: '/', component: Authorization },
     { path: '/sign-in', component: SignIn },
     { path: '/sign-up', component: SignUp },
-    { path: '/client', component: Client, meta: { toClient: true } },
-    { path: '/client/new-repair', component: NewRepair, meta: { toClient: true } },
-    { path: '/client/my-repairs', component: MyRepairs, meta: { toClient: true } },
-    { path: '/manager', component: Manager, meta: { toManager: true } },
-    { path: '/manager/:clientId', component: ManagerForUser, meta: { toManager: true } },
-    { path: '/set-repair-type/:clientId/:repairId', component: SetRepairType, meta: { toManager: true } },
-    { path: '/start-repair/:clientId/:repairId', component: StartRepair, meta: { toManager: true } }
+    { path: '/client', component: Client },
+    { path: '/client/new-repair', component: NewRepair },
+    { path: '/client/my-repairs', component: MyRepairs },
+    { path: '/manager', component: Manager },
+    { path: '/manager/:clientId', component: ManagerForUser },
+    { path: '/set-repair-type/:clientId/:repairId', component: SetRepairType },
+    { path: '/start-repair/:clientId/:repairId', component: StartRepair }
   ]
-})
-
-router.beforeEach((to, from, next) => {
-  const currentUser = firebase.auth().currentUser
-  const toClient = to.matched.some(record => record.meta.toClient)
-  const toManager = to.matched.some(record => record.meta.toManager)
-
-  if (currentUser) {
-      firebase.database().ref(`managers/${currentUser.uid}`).once('value', snap => {
-      const isManager = snap.val()
-      console.log(isManager, toManager)
-
-      if ((isManager && toManager) || (!isManager && toClient)) {
-        next()
-      } else if (isManager && toClient) {
-        next('/manager')
-      } else if (!isManager && toManager) {
-        next('/client')
-      }
-    })
-  }
-
-
-  // if (!currentUser) {
-  //   next('/')
-  // } else {
-  //   next()
-  // }
 })
 
 export default router
